6acf62ea5c4c599bb29fd65c9694c4de213bed17
FanRende <zxcj04@gmail.com>
2022-03-12 15:01:50 +0800

    Initial Commit

---
+++ Connect4/Connect4App.swift
@@ -0,0 +1,17 @@
+//
+//  Connect4App.swift
+//  Connect4
+//
+//  Created by FanRende on 2022/3/12.
+//
+
+import SwiftUI
+
+@main
+struct Connect4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
